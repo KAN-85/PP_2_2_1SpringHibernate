e79ba9c4ab42aed7f3ab7493c26981ae2f990162
KAN-85 <ak-9000@yandex.ru>
2023-11-08 14:37:13 +0400

    Add Car class Linked @OneToOne Car and User classes Add getUserByCar class

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDao.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDao.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface UserDao {
    void add(User user);
    List<User> listUsers();
+
+   Object getUserByCar(String model, int series);
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,7 +1,9 @@
 package hiber.dao;
 
+
 import hiber.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -11,19 +13,30 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 
+    public Object getUserByCar(String model, int series) {
+
+        final String hql = "FROM User user WHERE user.car.model = :model AND user.car.series = :series";
+
+        Query<User> query = sessionFactory
+                .getCurrentSession()
+                .createQuery(hql)
+                .setParameter("model", model)
+                .setParameter("series", series);
+        return query.setMaxResults(1).getSingleResult();
+    }
 }
